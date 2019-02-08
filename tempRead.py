307c871d2b0452c83d464b71445a87a2e245f417
Fisher <george.kokinis@gmail.com>
2019-02-08 19:43:45 +0000

    Little text change and fixing return value being string; should be ready to end sprint1

--- tempRead.py
+++ tempRead.py
@@ -3,25 +3,25 @@ import time
 
 
 def getSensors():
-    """Return all connected onewire sensors."""
-    return [dev for dev in os.listdir("/sys/bus/w1/devices/") if "28" in dev[0:2]]
+    """Return all connected OneWire devices."""
+    return [d for d in os.listdir("/sys/bus/w1/devices/") if "28" in d[0:2]]
 
 
 def readRawTemp(sensor):
-    """Get raw value for temperature (spec: DEG C *1000)"""
+    """Get raw value for temperature (spec: °C*1000)."""
     with open("/sys/bus/w1/devices/{}/w1_slave".format(sensor), "r") as f:
         data = f.readlines()
     return data
 
 
 def getTemp(sensor):
-    """Check CRC and convert to DEG C."""
+    """Check CRC and convert to °C."""
     data = readRawTemp(sensor)
     while data[0].strip()[-3:] != "YES":
         time.sleep(0.1)
         data = readRawTemp(sensor)
     (discard, sep, reading) = data[1].partition(" t=")
-    return reading / 1000
+    return float(reading) / 1000
 
 
 def main():
