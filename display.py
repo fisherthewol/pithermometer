8cb5fa22bb5923df5df840d7c549e543f906c0c4
Fisher <george.kokinis@gmail.com>
2019-03-04 21:57:05 +0000

    Sanity Check

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 display = InkyWHAT("black")
 img = Image.new("P", (display.width, display.height), display.WHITE)
-draw_context = ImageDraw(img)
+draw_context = ImageDraw.Draw(img)
 font = ImageFont.truetype("RobotoSlab-Regular.ttf", 22)
 draw_context.text((20, 20), "28-00000a81ec36: 18.5°C", display.BLACK, font)
 display.set_image(img)
