d17a2c3e83916377f6944d90ee5684a10b1be4e8
Fisher <george.kokinis@gmail.com>
2019-03-05 19:30:06 +0000

    Begin display output

--- display.py
+++ display.py
@@ -1,13 +1,11 @@
 import models
-# from inky import InkyWHAT
-# from PIL import Image, ImageDraw, ImageFont
+from inky import InkyWHAT
+from PIL import Image, ImageDraw, ImageFont
 from tempRead import getSensors
 
 
-# display = InkyWHAT("black")
-# img = Image.new("P", (display.width, display.height), display.WHITE)
-# draw_context = ImageDraw.Draw(img)
-# roboto = ImageFont.truetype("RobotoSlab-Regular.ttf", 22)
+display = InkyWHAT("black")
+roboto = ImageFont.truetype("RobotoSlab-Regular.ttf", 22)
 # draw_context.text((20, 20),
 #                   "28-00000a81ec36: 18.5°C",
 #                   display.BLACK,
@@ -16,16 +14,24 @@ from tempRead import getSensors
 # display.show()
 
 
+def drawScreen(loftemps):
+    img = Image.new("P", (display.width, display.height), display.WHITE)
+    draw_context = ImageDraw.Draw(img)
+
+
 def main():
     # while True:
     sensors = getSensors()
+    temps = {}
     for sensor in sensors:
         with models.db:
             query = (models.Reading.select()
                      .where(models.Reading.sensor == sensor)
                      .order_by(models.Reading.timestamp.desc()))
-        print("Sensor {}: {}°C".format(sensor, query[0].temperature))
-    return
+        temps[sensor] = query[0].temperature
+    drawScreen(temps)
+        # print("Sensor {}: {}°C".format(sensor, query[0].temperature))
+    # return
 
 
 if __name__ == "__main__":
