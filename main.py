80f43202550afa73a02eac26fccc7e54df82a03a
Fisher <george.kokinis@gmail.com>
2019-03-18 17:49:07 +0000

    Refer to function, don't call it.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import time
 
 if __name__ == "__main__":
     jobs = []
-    p = multiprocessing.Process(target=tempRead.main())
+    p = multiprocessing.Process(target=tempRead.main)
     jobs.append(p)
-    p2 = multiprocessing.Process(target=display.main())
+    p2 = multiprocessing.Process(target=display.main)
     jobs.append(p2)
     for job in jobs:
         job.start()
